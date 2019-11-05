94a91a5e0e3333b273cbb8ecdd44da7542e0fda5
saramarin23 <saramarin.econ@gmail.com>
2019-11-05 22:46:37 +0100

    create header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import Header from "./components/header/Header";
 import HomePage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shop/Shop";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         {/* Exact sin nada sería true, luego sería el path exacto */}
