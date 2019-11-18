079e0f687cb505547ca1b4c69861754409a1c48e
saramarin23 <saramarin.econ@gmail.com>
2019-11-18 13:32:16 +0100

    update app with reselect, replace cart with selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from "reselect";
+
+const selectCart = state => state.cart;
+//It's gonna get the whole reducer state and we just want the cart
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
